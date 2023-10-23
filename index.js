7b85a193a62b4b3f5dcef7cc25ef61f464ce2b0b
arzumanov2004 <sd8cxz70x@code.edu.az>
2023-10-23 21:30:06 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,175 @@
+// 1-ci sual 
+
+
+
+
+// let a = 5;
+// let b = 8;
+// let c = 3;
+// let discriminant;
+// discriminant = b * b - 4 * a * c;
+// let root1;
+// let root2;
+// if (discriminant > 0) {
+//     root1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+//     root2 = (-b + Math.sqrt(discriminant))/ (2 * a);
+//     console.log(root1,root2);
+// }
+// else if (discriminant == 0){
+//     root1 = -b/2*a;
+//     console.log(root1);
+// }
+// else{
+//     console.log(heqiqi koku yoxdur);
+// }
+
+// 2-ci sual.
+
+
+// let a = 5
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % a === 0) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+
+// 3-cu sual. 
+
+
+// const arr = [2, 4, 5, 9, 1];
+// const min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         console.log(arr[i]);
+    
+//     }
+    
+// }
+
+
+
+
+
+// 4-cu sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+// const max = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         console.log(arr[i]);
+//     }
+    
+// }
+
+
+
+
+// 5-ci sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+
+// const min = arr [0];
+// const index = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+
+// 6-ci sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+// const max = arr[0];
+// const index = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+
+// 7-ci sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+// let cem = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 === 1) { 
+//     cem += arr[i];
+//   }
+// }
+
+// console.log(cem);
+
+
+
+
+// 8-ci sual. 
+
+
+
+// let sentence = "Mən Code Academy Tələbəsiyəm";
+// let lowerCaseSentence = sentence.toLowerCase();
+// console.log(lowerCaseSentence);
+
+
+
+
+
+
+
+// 9-cu sual. 
+
+
+
+// let sentence =  "            Mən Code Academydə Programing tədrisi alıram            ";
+
+// console.log(sentence.trimStart());
+
+
+
+
+// 10-cu sual. 
+
+
+
+
+// let myEducation = "Mən Code Academy Tələbəsiyəm";
+// let words = myEducation.split(" ");
+
+// for (let i = 0; i < words.length; i++) {
+    
+//     console.log(words[i]);
+    
+// }
+
+
+
+
+// 11-ci sual. 
+
+
+// let sentence = "            Mən Code Academydə Programing tədrisi alıram            "
+// let trimmedSentence = sentence.trim();
+// let words = trimmedSentence.split(" ");
+// let pureSentence = words.join("");
+// console.log(pureSentence.length);
